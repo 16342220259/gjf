9f8c3946e361f09f4cdd35053e8b54a1bd82ee7c
16342220259 <59324757+16342220259@users.noreply.github.com>
2023-01-23 23:14:53 +0800

    Update py0.lua

--- py0.lua
+++ py0.lua
@@ -126,7 +126,86 @@ wshsaddr=so+0x7CC0D0--无视海水>>> ✓
 --特征码2,172,709,610,637,574,144 XA Q类型 本地址 改506630144D
 
 
-
+mrrw =  sddz+0x22904C
+ dianhuo =  sddz+0x16364DC
+ Any_daddr =  sddz+0x160F818
+ yclzaddr =  sddz+0x1B7D07C
+ huazb =  sddz+0x21798F0
+ ydgypy =  sddz+0x1B4B8EC
+ Azhahua =  sddz+0x2470B38
+ friendship =  sddz+0x20EC80
+ xhaddr =  sddz+0x497714
+ LightChipAddr =  sddz+0x21C2ECC
+ luzhikg =  sddz+0x22E2E48
+ daoli =  sddz+0x4945C0
+ dtaddr =  sddz+0x22E9DB0
+ Putdz =  sddz+0x306F2C
+ longzero =  sddz+0x105EB58
+ yunaddr =  sddz+0x15837FC
+ julz =  sddz+0x1B86128
+ ktpd =  sddz+0x1ABF8A0
+ ktdz =  sddz+0x1BA64A8
+ ysdz = rwdz+0x1EE0
+ dpaddr = rwdz+0x52A4
+ lgdz = rwdz+0x61A0
+ lzsj = rwdz+0x11C4A4C
+ mfdz = rwdz+0x11384
+ zspy = rwdz+0x344C
+ zhayi = rwdz+0x530C
+ lazushuliang = rwdz+0x9944
+ yanhuadz = rwdz+0x87CC
+ wxyq = rwdz+0x5264
+ nengliang = rwdz+0x5250
+ dataddr = rwdz+0x86A4
+ Address_clos = rwdz+0x714C
+ dengjiaddr = rwdz+0x11E78
+ wardrobe = rwdz-0x22934
+ zhongli = rwdz-0x2FC4
+ dengjiaddr = rwdz+0x8684
+ dxbaddr = rwdz-0x2370AC
+ dxbaddr1 = rwdz-0x2285A8
+ fjrs = rwdz-0x22857C
+ sxdz = rwdz-0x1F44BC
+ hyjc = rwdz-0x2235BC
+ hyxx = rwdz+0x11A51AC
+ zf = rwdz+0x11A51E4
+ mfkg = rwdz+0x11A51F4
+ mfkz = rwdz-0x21F7FC
+ mfjh = rwdz+0x15780B7B
+ XZDZ = rwdz+0x1657D648
+ ygdz = rwdz+0x158B7F0C
+ uidz = rwdz+0x15DEC887
+ Earphone = rwdz+0x15B50278
+ andz = rwdz+0x15B6666C
+ qcst = rwdz+0x15EEC550
+ wsst = rwdz+0x15EEC550
+ wxsf =  sddz+0x2188C5C
+ kp1 = rwdz+0x1750E920
+ ltkb2 = rwdz+0x1750E920
+ ltkb3 = rwdz+0x167CC9F0
+ yedpy = rwdz+0x15A5DF0C
+ qfqdfz = rwdz+0x15FDAB40
+ xacq = rwdz+0x159B6F1C
+ zzbc = rwdz+0x15CE29B0
+ shuisp = rwdz+0x167CC9F0
+ guangaddr = rwdz+0x158D2B70
+ rwpzz = rwdz+0x15A6DAA8
+ huabx = rwdz+0x167C2044
+ cao = rwdz+0x158CEC50
+ dogshin = rwdz+0x167B44E8
+ zhdxaddr = rwdz+0x165C4E5C
+ hjps = rwdz+0x167EC4A4
+ cjty = rwdz+0x15776B50
+ dzsja = rwdz+0x15E0549C
+ dzsjb = rwdz+0x167BFB9C
+ gfs = rwdz+0x158C1190
+ xsxh = rwdz+0x15CE1DC8
+ hxjs = rwdz+0x15880790
+ wxsj = rwdz+0x15A682A0
+ xadh = rwdz+0x158B345C
+ dpfg =  sddz+0x1B4C71C
+  
+  
  -- Teleport({1.2416000366211;0.20657592773438;6.118523025512695}) 
   --Teleport({1.2416000366211;0.20657592773438;6.118523025512695}) 
 end
