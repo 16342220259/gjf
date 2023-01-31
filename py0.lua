2615f7efc4f848e6cb3265d9714f1c91796980a4
16342220259 <59324757+16342220259@users.noreply.github.com>
2023-02-01 00:59:12 +0800

    Update py0.lua

--- py0.lua
+++ py0.lua
@@ -11,7 +11,7 @@ rw3=addrjump(rw2,0)+0x30
 rw4=addrjump(rw3,0)+0x1dc
 rwdz=rw4
 
-soo=so+0x1476000--cd
+soo=so+0x1477000--cd
 --1015 xa-cd=13A3000 ca-cb=13FB000
 sddz=addrjump(soo,0x13260)+0x1c4
 gg.addListItems({{address=sddz,flags=16,name='速度'}})
