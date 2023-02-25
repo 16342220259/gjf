df41beca9bbea8083da00d17b2a8328c898afc8e
1634220259@qq.com <1634220259@qq.com>
2023-02-25 17:42:29 +0100

    1

--- py0.lua
+++ py0.lua
@@ -8,7 +8,6 @@ sooo=gg.getRangesList('libBootloader.so:bss')[1].start  --cb
 rw1 = sooo
 rw2=addrjump(rw1,0x8d4b8)+0x10
 rw3=addrjump(rw2,0)+0x30
-rw4=addrjump(rw3,0)+0x1ec
 rwdz=rw4
 
 soo=so+0x15b1000--cd
@@ -18,7 +17,7 @@ gg.addListItems({{address=sddz,flags=16,name='速度'}})
 
 ------------核心偏移---------
 csaddr = sddz+ 0x16107A8--任意门>>> ✓
-dtaddr = sddz + 0x22ECF50 ---地图判断>> ✓   
+dtaddr = sddz + 0x22973c0 ---地图判断>> ✓   
 ydgypy = sddz + 0x1b4b8ec ---原地光翼>>> ✓  :l_CandleSpace_0 往前两个
 dengjiaddr = rwdz-0x22934--光翼数量>>> ✓
 mrrw=sddz + 0x229F2C---原地任务>>>   107,374,182,400;25D;4,294,967,296;1D::17从1D转找到有1~288D的1D地址
