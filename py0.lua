04def24ac1f275fb94f2ab90f3715aad503b8ea2
16342220259 <59324757+16342220259@users.noreply.github.com>
2023-04-16 16:11:44 +0800

    Update py0.lua

--- py0.lua
+++ py0.lua
@@ -6,7 +6,7 @@ gg.addListItems({{address=gg.getRangesList('libBootloader.so')[1].start,flags=32
 sooo=gg.getRangesList('libBootloader.so:bss')[1].start  --cb  
 
 rw1 = sooo
-rw2=addrjump(rw1,0x8f038b8)+0x10
+rw2=addrjump(rw1,0x8f038)+0x10
 rw3=addrjump(rw2,0)+0x30
 rw4=addrjump(rw3,0)+0x20c
 rwdz=rw4
