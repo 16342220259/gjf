852dbaf3354336a027c15fa49cf5177a531a2914
16342220259 <59324757+16342220259@users.noreply.github.com>
2023-06-04 13:33:45 +0800

    Update py0.lua

--- py0.lua
+++ py0.lua
@@ -22,9 +22,9 @@ sddz=addrjump(soo,0x14ca0)+0x1c8
 gg.addListItems({{address=sddz,flags=16,name='速度'}})
 
 ------------核心偏移---------
-csaddr = sddz  + 0x17063D4-任意门>>> ✓
+csaddr = sddz  + 0x17063D4 --任意门>>> ✓
   
-dtaddr = sddz + 0x230809c---地图判断>> ✓   
+dtaddr = sddz + 0x230809c ---地图判断>> ✓   
 
 ydgypy = sddz + 0x1c6fb58---原地光翼>>> ✓  :l_CandleSpace_0 往前两个
   ydgyhqa = ydgypy + 0x58
