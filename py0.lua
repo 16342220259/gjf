5de3cebe0dc8480efb37df5a982ce069e4c314bb
16342220259 <59324757+16342220259@users.noreply.github.com>
2023-07-14 23:43:25 +0800

    Update py0.lua

--- py0.lua
+++ py0.lua
@@ -26,7 +26,7 @@ gg.addListItems({{address=sddz,flags=16,name='速度'}})
   csaddr = sddz  + 0x1705D54--任意门>>> ✓
   Any_daddr =  sddz + 0x1705d54
   
-dtaddr = sddz + 0x230809c ---地图判断>> ✓   
+dtaddr = sddz + 0x23405bc ---地图判断>> ✓   
 
 ydgypy = sddz + 0x1c6fb58---原地光翼>>> ✓  :l_CandleSpace_0 往前两个
   ydgyhqa = ydgypy + 0x58
